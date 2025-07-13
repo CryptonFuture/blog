6212793c103643deded709b2a2d653430ae4e338
CryptonFuture <crypton.futuremedia1989@gmail.com>
2025-07-13 11:48:36 +0500

    for name change

--- js/main.js
+++ js/main.js
@@ -29,11 +29,11 @@
                     <div class="modal-body">
                         <form>
                             <div class="mb-3">
-                                <label for="loginEmail" class="form-label">Email address</label>
+                                <label for="email" class="form-label">Email address</label>
                                 <input type="email" class="form-control" id="email" placeholder="Enter your email">
                             </div>
                             <div class="mb-3">
-                                <label for="loginPassword" class="form-label">Password</label>
+                                <label for="password" class="form-label">Password</label>
                                 <input type="password" class="form-control" id="password" placeholder="Enter your password">
                             </div>
                             <div class="mb-3 form-check">
