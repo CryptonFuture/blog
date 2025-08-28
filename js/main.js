6c71abd2ff440375c7252f0ec038fe2bea6c87c6
CryptonFuture <crypton.futuremedia1989@gmail.com>
2025-08-28 15:17:34 +0500

    role api is integrated

--- js/main.js
+++ js/main.js
@@ -16,91 +16,60 @@
     $('#sidebar').toggleClass('active');
   });
 
-  $(document).ready(function () {
-    $('#openModal').on('click', function () {
-      const ModalHtml = `
-                <div
-        class="modal fade"
-        id="Modal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Login Form
-              </h1>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body">
-              <form id="form-submit">
-                <div class="mb-3">
-                  <label for="recipient-name" class="col-form-label"
-                    >Email:</label
-                  >
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                  />
-                </div>
-                <small id="email-error" class="text-danger"></small>
-                <div class="mb-3">
-                  <label for="message-text" class="col-form-label"
-                    >Password:</label
-                  >
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                  />
-                </div>
-                 <small id="password-error" class="text-danger"></small>
-                 <small class="float-right"><a href="request_form.html">Request Form</a></small>
-                 <div class="mb-3">
-                    <label for="edit-post-status" class="col-form-label">Remember me:</label>
-                      <div class="form-check mb-3">
-                        <input class="form-check-input" type="checkbox" id="rememberMe" name="publish">
-                          <label class="form-check-label" for="publishCheck">
-                          </label>
-                        </div>
-                        </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button id="submit" onclick="login()" type="button" class="btn btn-primary">Submit</button>
-            </div>
-          </div>
-        </div>
-      </div>
-        `;
+  // $(document).ready(function () {
+  //   $('#openModal').on('click', function () {
+  //     const ModalHtml = `
+               
+  //       `;
 
-      $('body').append(ModalHtml)
+  //     $('body').append(ModalHtml)
 
-      const modal = new bootstrap.Modal(document.getElementById('Modal'))
-      modal.show()
+  //     const modal = new bootstrap.Modal(document.getElementById('Modal'))
+  //     modal.show()
 
-      $('#Modal').on('hidden.bs.modal', function () {
-        $(this).remove()
-      })
-    })
-  })
+  //     $('#Modal').on('hidden.bs.modal', function () {
+  //       $(this).remove()
+  //     })
+  //   })
+  // })
 
 })(jQuery);
+document.addEventListener('DOMContentLoaded', function () {
+  getRole()
+})
+
+const tokenType = localStorage.getItem('tokenType')
+const access_Token = localStorage.getItem('token')
+
+async function getRole() {
+	
+	const res = await fetch(`${baseUrl}/getRoles`, {
+		method: 'GET'
+	})
+
+	const data = await res.json()
+
+	const role = data.data
+
+	const rolelist = document.getElementById('role')
+
+	rolelist.innerHTML = '';
+
+	if (!data.success || !data.data || data.data.length === 0) {
+			 const errorRow = `<option disabled selected>${data.error || "No record found"}</option>`;
+			  rolelist.innerHTML = errorRow
+			return ;
+	}
+
+  rolelist.innerHTML = `<option value="" disabled selected>Select Role</option>`;
+
+	role.forEach((item, index) => {
+		rolelist.innerHTML += `
+				<option value="${item.role}">${item.name}</option>
+			`
+	})
+}
+
   const secretKey = "0192384756";
   
   window.addEventListener('DOMContentLoaded', () => {
@@ -128,9 +97,13 @@ const baseUrl = `http://localhost:8000/${prefix}`
 
 
 async function login() {
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
-   const rememberMe = document.getElementById('rememberMe').checked;
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+  const rememberMe = document.getElementById('rememberMe').checked;
+
+  const role = document.getElementById('role').value
 
   document.getElementById('email-error').textContent = ""
   document.getElementById('password-error').textContent = ""
@@ -163,7 +136,8 @@ async function login() {
     },
     body: JSON.stringify({
       email,
-      password
+      password,
+      role
     })
   })
 
@@ -180,6 +154,7 @@ async function login() {
     localStorage.setItem('email', data.user.email);
     localStorage.setItem('firstname', data.user.firstname);
     localStorage.setItem('lastname', data.user.lastname);
+    localStorage.setItem('role', data.user.role);
 
     localStorage.setItem('tokenType', data.user.tokenType);
 
